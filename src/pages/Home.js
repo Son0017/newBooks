f6717f15e44bdf31bb504a59d53981c3bf558eb7
Son0017 <soneec.017@gmail.com>
2023-05-31 15:34:50 +0500

    s

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import CocktailList from "../components/CocktailList";
 import { useState } from "react";
-// import { useSelector } from "react-redux";
 import { useBooksProvider } from "../context/book_context";
 const Home = () => {
   const { categoryList } = useBooksProvider();
